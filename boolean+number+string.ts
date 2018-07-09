2adb309dbe3dc48fcff81d0d6e17f00c135c5be6
slawiko <shchaurouski.slava@gmail.com>
2018-07-09 08:46:57 +0300

    initial commit (work in progress)

---
+++ boolean+number+string.ts
@@ -0,0 +1,7 @@
+const isAnton: boolean = false;
+const man = 'Anton';
+const n: number = 10;
+
+const greeting: string = `Hello, ${isAnton ? man : 'nowhere man'} ${n} times`;
+
+console.log(greeting);
---
+++ dist/boolean+number+string.js
@@ -0,0 +1,6 @@
+var isAnton = false;
+var man = 'Anton';
+var n = 10;
+var greeting = "Hello, " + (isAnton ? man : 'nowhere man') + " " + n + " times";
+console.log(greeting);
+//# sourceMappingURL=boolean+number+string.js.map
